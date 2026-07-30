94c37a9332f4de3222afeed9050b6fa848767a29
w1llx1m <williamsilva.monteiro28@gmail.com>
2026-07-29 22:36:20 -0300

    transforming data

--- scripts/transform.py
+++ scripts/transform.py
@@ -1,9 +1,13 @@
 import pandas as pd
+import datetime as dt
 import re
 
 
 def  capitalize_title(df: pd.DataFrame) -> pd.Series:
-    pass
+    cols_ToCapitalize = ['first_name', 'last_name', 'city']
+    return df[cols_ToCapitalize].all(axis=1).astype(str).str.capitalize()
 
 def date_format(df: pd.DataFrame) -> pd.Series:
+    # date_ToFormat = ['invoice_date'] #dd/mm/yyyy
     pass
+
